c7393cd1d9651d3838d8eafeec378a4eb9cd88c4
unknown <eduardcolmenares2236@gmail.com>
2021-03-27 23:05:26 -0430

    maquetando el proyecto

--- index.php
+++ index.php
@@ -1,84 +1,27 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda</title>
-    <link rel="stylesheet" href="assets/css/styles.css"/>
-</head>
-<body>  
-        <!--HEADER-->
-        <header id="header">
-            <div id="logo" >
-                <img src="assets/img/camiseta.png" alt="logo de camiseta" />
-                <a href="index.php"> Tienda de camisetas </a>
-            </div>
-        </header>
+<h1>bienvenidos a mi web con MVC</h1>
+<?php
+require_once'autoload.php';
 
-        <!--MAIN-->
-        <nav>
-            <ul>
-                <li>
-                    <a href="#" >categoria 1</a>
-                </li>
-                <li>
-                    <a href="#" >categoria 2</a>
-                </li>
-                <li>
-                    <a href="#" >categoria 3</a>
-                </li>
-                <li>
-                    <a href="#" >categoria 4</a>
-                </li>
-                <li>
-                    <a href="#" >categoria 5</a>
-                </li>
-                <li>
-                    <a href="#" >categoria 6</a>
-                </li>
-            </ul>
-        </nav>
-        <div id="content">
-        <!--SIDEBAR-->
-        <aside id="lateral">
-            <div id="login" class="block_aside">
-                <form>
-                    <label for="email">Email</label>
-                    <input type="email" name="email"/>
-                    
-                    <label for="password">Contraseña</label>
-                    <input type="password" name="password"/>
-                    
-                    <input type="submit" value="Ingresar"/>
+if(isset($_GET['controller'])){
+    $nombre_controlador = $_GET['controller'].'Controller';
+}else{
+    echo "la pagina no existe";
+    exit();
+}
 
-                    <a href="#">mis pedidos</a>
-                    <a href="#">gestionar pedidos</a>
-                    <a href="#">gestionar categorias</a>
-                </form>
-            </div>
-        </aside>
-        <!--central content-->
-            <div id="central">
-                <div class="product">
-                    <img src="assets/img/camiseta.png">
-                    <h2>camiseta suave</h2>
-                    <p>30 dolares</p>
-                    <a href="#">Comprar</a>
-                </div>
 
-                <div class="product">
-                    <img src="assets/img/camiseta.png">
-                    <h2>camiseta suave</h2>
-                    <p>30 dolares</p>
-                    <a href="#">Comprar</a>
-                </div>
-
-            </div>
-        </div>
-        <!--footer-->
-        <footer>
-            <p> Desarrollado por Eduard Colmenares &copy; <?= date('y')?> </p>
-        </footer>
-</body>
-</html>
+if(class_exists($nombre_controlador)){
+    $controlador = new $nombre_controlador();
+    
+    if(isset($_GET['action']) && method_exists($controlador,$_GET['action'])){
+        $action = $_GET['action'];
+        $controlador->$action();
+    
+    }else{
+        echo " la pagina que buscas no existe";
+    }
+    
+}else{
+    echo "la pagina no existe";
+}
+?>
---
+++ index_maqueta.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
+    <link rel="stylesheet" href="assets/css/styles.css"/>
+</head>
+<body>  
+        <!--HEADER-->
+        <div id="container">
+        <header id="header">
+            <div id="logo" >
+                <img src="assets/img/camiseta.png" alt="logo de camiseta" />
+                <a href="index.php"> Tienda de camisetas </a>
+            </div>
+        </header>
+
+        <!--MAIN-->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#" >categoria 1</a>
+                </li>
+                <li>
+                    <a href="#" >categoria 2</a>
+                </li>
+                <li>
+                    <a href="#" >categoria 3</a>
+                </li>
+                <li>
+                    <a href="#" >categoria 4</a>
+                </li>
+                <li>
+                    <a href="#" >categoria 5</a>
+                </li>
+                <li>
+                    <a href="#" >categoria 6</a>
+                </li>
+            </ul>
+        </nav>
+        <div id="content">
+        <!--SIDEBAR-->
+        <aside id="lateral">
+            <div id="login" class="block_aside">
+                <h3>Entrar a la web</h3>
+                <form>
+                    <label for="email">Email</label>
+                    <input type="email" name="email"/>
+                    
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password"/>
+                    
+                    <input type="submit" value="Ingresar"/>
+                    <ul>
+                           <li><a href="#">mis pedidos</a></li>
+                           <li><a href="#">gestionar pedidos</a></li>
+                           <li><a href="#">gestionar categorias</a></li>
+                    </ul>
+                </form>
+            </div>
+        </aside>
+        <!--central content-->
+            <div id="central">
+            <h1>productos destacados</h1>
+                <div class="product">
+                    <img src="assets/img/camiseta.png">
+                    <h2>camiseta suave</h2>
+                    <p>30 dolares</p>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+
+                <div class="product">
+                    <img src="assets/img/camiseta.png">
+                    <h2>camiseta suave</h2>
+                    <p>30 dolares</p>
+                    <a href="#"class="button">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/camiseta.png">
+                    <h2>camiseta suave</h2>
+                    <p>30 dolares</p>
+                    <a href="#"class="button">Comprar</a>
+                </div>
+
+            </div>
+        </div>
+        <!--footer-->
+        <footer id="footer">
+            <p> Desarrollado por Eduard Colmenares &copy; <?= date('Y')?> </p>
+        </footer>
+        </div>
+</body>
+</html>
